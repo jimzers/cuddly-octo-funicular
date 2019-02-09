dfe37322cc1df37267fef173871f698e645808bb
jimzers <adamleesaintfrancis@gmail.com>
2019-02-09 10:40:40 -0800

    init layout and home views, init styles.css

---
+++ web-hub/resources/views/partials/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
