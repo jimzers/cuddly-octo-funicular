9edde1590f27ccbd493d7431fa9d23bf786dc598
WeebHunter4 <47472299+WeebHunter4@users.noreply.github.com>
2019-02-09 17:59:15 -0800

    modified styles.css

--- web-hub/resources/views/blogposts/show.blade.php
+++ web-hub/resources/views/blogposts/show.blade.php
@@ -1,10 +1,22 @@
+
 @extends('partials.layout')
+
 
 @section('content')
 
     <!-- TODO: this should be a page where you can see the article. think of it like new york times or something.
     make it look fancy -->
+    <link rel="stylesheet" type="text/css" href="/web-hub/public/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/styles.css">
+
+
+
     <h1 class="title">{{ $blogpost->title }}</h1>
+    <p class="author"> {{$blogpost->author}}</p
+
+    <hr/>
+
+    <p class="text"> {{ $blogpost->content}}</p> 
 
 
 
