d5b3968f806e22b90cee26d5777c7b28e4aac2a0
WeebHunter4 <47472299+WeebHunter4@users.noreply.github.com>
2019-02-09 18:46:01 -0800

    more show.blade UI things

--- web-hub/resources/views/partials/layout.blade.php
+++ web-hub/resources/views/partials/layout.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head style="background-color: black;">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <title>Apptivism</title>
@@ -8,11 +8,27 @@
 <body>
 	<div class="container">
    <nav class="navbar navbar-expand-sm bg-light navbar-light">
-  		<h2 class=> Apptivism </h2>
-	</nav>
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link nav-text" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link nav-text" href="#">Mission</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link nav-text" href="#">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link nav-text" href="#">Contact Us</a>
+    </li>
+  </ul>
+
+</nav>
 </div>
+
 <div class="container">
     @yield('content')
 </div>
+
 </body>
 </html>
